f14b09ee52c38cd42905c96ddb4e4238c6f83c83
heetsantoki <heetsantoki16@gmail.com>
2026-03-23 18:01:17 +0530

    fix: allow all vercel deployments in CORS

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+//app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
