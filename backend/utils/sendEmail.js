1683283ba4787cafce1aa4e8d3a5dadf41291847
heetsantoki <heetsantoki16@gmail.com>
2026-05-13 16:41:52 +0530

    bug fixes

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
-  secure: true, // use SSL
+  port: 587,
+  secure: false, // use SSL
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
